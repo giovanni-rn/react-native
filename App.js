a7fa2ebc7bd5e889cd9af2b7191cbba5929274b1
Reamu77 <reamutarzan@gmail.com>
2023-02-19 09:08:21 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const alertText = () => {
+    alert(text);
+    setText("");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <Text style={styles.block}>
+        Lancer une alerte avec le texte ci-dessous 👇
+      </Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="texte à afficher"
+        onChangeText={setText}
+        defaultValue={text}
+      />
+      <Button title="Alerte !" onPress={alertText} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "whitesmoke",
+  },
+  textInput: {
+    margin: 15,
+    padding: 7,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "black",
   },
 });
